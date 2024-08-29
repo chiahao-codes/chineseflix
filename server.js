0a6f77d33149b9398e0c445362b47c1f89566f9e
Chiahao <chiahao.codes@gmail.com>
2024-08-28 21:22:45 -0700

    main

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+let PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("assets"));
+app.use(express.json());
+app.use(cors({ origin: "*", credentials: true }));
+
+app.listen(PORT, () => {
+  console.log(`app listening on Port: ${PORT}`);
+});
