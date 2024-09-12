a20c02ff0d5d815eb0094191e83aeaf511209dcd
Chiahao <chiahao.codes@gmail.com>
2024-09-11 20:36:00 -0700

    commit main

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 const app = express();
 
 let PORT = process.env.PORT || 8080;
+const DOMAIN = process.env.DOMAIN;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -11,7 +12,15 @@ app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 
 app.get("/", (req, res, next) => {
-  res.render("home");
+  res.render("home", { DOMAIN });
+});
+
+app.get("/about", (req, res, next) => {
+  res.render("about");
+});
+
+app.get("/login", (req, res, next) => {
+  res.render("login");
 });
 
 app.listen(PORT, () => {
