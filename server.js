7b1bf63e21e99aa086c8ddfeb46507cc71a95ae9
Chiahao <chiahao.codes@gmail.com>
2024-10-14 15:23:58 -0700

    commit main

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const mongoUser = encodeURIComponent("chiahao1");
+const mongoPW = encodeURIComponent("xZBiOUuUGJg2GT0I");
+const uri = `mongodb+srv://${mongoUser}:${mongoPW}@chineseflix.r2ugzun.mongodb.net/?retryWrites=true&w=majority&appName=CHINESEFLIX`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("CHINESEFLIX").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
 const app = express();
 
 let PORT = process.env.PORT || 8080;
@@ -42,7 +70,7 @@ app.get("/terms", (req, res, next) => {
 app.get("/account", (req, res, next) => {
   res.render("account");
 });
-
+run().catch(console.dir);
 app.listen(PORT, () => {
   console.log(`app listening on Port: ${PORT}`);
 });
